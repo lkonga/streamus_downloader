5a9daed2ed5b3f094b5b1ea5a811d70f482a8112
lkonga <cmsolution@gmail.com>
2016-03-28 05:42:32 +0700

    first commit

---
+++ test.sh
@@ -0,0 +1,19 @@
+for entry in "`pwd`"/*.json
+do
+    if [ -f "$entry" ];then
+        grep -o 'https://[^"]*' $entry | sort | uniq > "nodupes/"${entry##*/}
+    fi
+done
+
+
+for entry in "`pwd`"/nodupes/*.json
+do
+    if [ -f "$entry" ];then
+        FILENAME=${entry##*/}
+        BASENAME=${FILENAME%.*}
+        youtube-dl -a "$entry" -i -o "downloads/$BASENAME/%(title)s-%(id)s.%(ext)s"
+    fi
+done
+
+
+

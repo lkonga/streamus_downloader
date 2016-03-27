b1e6a689c78d1044cc0fbfec0c656491938f2acf
lkonga <cmsolution@gmail.com>
2016-03-28 06:21:43 +0700

    fix escapes on paths

--- streamus_dler.sh
+++ streamus_dler.sh
@@ -1,7 +1,7 @@
 for entry in "`pwd`"/*.json
 do
     if [ -f "$entry" ];then
-        grep -o 'https://[^"]*' $entry | sort | uniq > "nodupes/"${entry##*/}
+        grep -o 'https://[^"]*' "$entry" | sort | uniq > "nodupes/"${entry##*/}
     fi
 done
 
@@ -9,8 +9,9 @@ done
 for entry in "`pwd`"/nodupes/*.json
 do
     if [ -f "$entry" ];then
+        echo $entry
         FILENAME=${entry##*/}
-        BASENAME=${FILENAME%.*}
+        BASENAME="${FILENAME%.*}"
         youtube-dl -a "$entry" -i -o "downloads/$BASENAME/%(title)s-%(id)s.%(ext)s"
     fi
 done

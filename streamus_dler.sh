1ba63b1feb4b83d742ac222c78c0d8eb62939f73
lkonga <cmsolution@gmail.com>
2016-03-28 13:56:43 +0700

    fix

--- streamus_dler.sh
+++ streamus_dler.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
 
-dir="nodupes"
+dir="done"
 if [ ! -d $dir ];then
     mkdir $dir
-fi
-
-dir2="done"
-if [ ! -d $dir2 ];then
-    mkdir $dir2
 fi
 
 for entry in "`pwd`"/*.json
 do
     if [ -f "$entry" ];then
-        grep -o 'https://[^"]*' "$entry" | sort | uniq > "$dir/${entry##*/}"
-        noduped="$dir/${entry##*/}"
+        grep -o 'https://[^"]*' "$entry" | sort | uniq > "noduped_${entry##*/}"
+        noduped="noduped_${entry##*/}"
         if [ -f "$noduped" ];then
-            FILENAME=${noduped##*/}
+            FILENAME=${entry##*/}
             BASENAME="${FILENAME%.*}"
             youtube-dl -a "$noduped" -i -o "downloads/$BASENAME/%(title)s-%(id)s.%(ext)s"
-            mv "$entry" "$dir2"
-            mv "$noduped" "$dir2/nodupes_$noduped"
+            mv "$entry" "$dir"
+            mv "$noduped" "$dir"
         fi
     fi
 done
